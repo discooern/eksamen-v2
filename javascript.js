e67c548b7ad3dd2b016b4b3c91a3d9a5b90c04e5
discooern <jona834n@edu.campusvejle.dk>
2021-03-10 14:32:31 +0100

    made the css and javascript a little nicer to look at

--- javascript.js
+++ javascript.js
@@ -1,16 +1,19 @@
+// gets 2 classes from index.html and stores them in variables
 var btn = document.querySelector('.hamburgerMenu');
 var hamMenu = document.querySelector('.links');
 let isOpen = false;
 
+// Gives the element with class hamburgerMenu a click function
 btn.addEventListener('click', function() { 
+    // Sets a variable to the numeral value of .links (width)
    let hamMenuWidth = window.getComputedStyle(hamMenu).width;
-   // opens menu
+    // opens menu
     if(!isOpen){
       btn.style.background = '#ce643e';
       hamMenu.style.animationName = "linksAnimation";
       isOpen = true;
       console.log("open");
-      // closes menu
+    // closes menu
     } else {
       btn.style.background = '#FE7D4E';
       hamMenu.style.animationName = "linksAnimation2";
